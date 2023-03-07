2b568e24b0de725a9b47a5a8fd8f0be5ef108e1c
AlexGol005 <pesok1985>
2023-03-07 10:25:01 +0300

    цу

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,13 @@ from django.urls import reverse
 class Blog(models.Model):
     date = models.DateField('Дата', auto_now_add=True)
     title = models.CharField('Заголовок', max_length=10000)
+    metatitle = models.CharField('Метазаголовок страницы', max_length=10000, blank=True, null=True)
+    description = models.TextField('Метаописание страницы', blank=True, null=True)
+    keywords = models.TextField('Ключевые слова', blank=True, null=True)
     text = models.TextField('Текст записи')
+
+
+
 
     def __str__(self):
         return f' {self.date} , {self.title}'
