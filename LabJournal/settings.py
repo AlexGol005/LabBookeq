0381ed845947b2465099f85df84fcd229ffa9e2b
AlexGol005 <pesok1985>
2024-01-05 18:47:39 +0300

    23080501

--- LabJournal/settings.py
+++ LabJournal/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = "vs3umg+heg!)ljfeg2o^eu@3fk1ma!=ett2^9o2x8cwjl@0ixy"
 # DEBUG = True
 # ALLOWED_HOSTS = []
-ADMINS = [('Alex', 'sandra.005@mail.ru')]
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
 EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "sandra.005@mail.ru"
