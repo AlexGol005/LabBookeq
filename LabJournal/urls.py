d703b4e463d197f2527dacbcb1c8ab796b2d0555
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-05-08 15:16:39 +0300

    Update urls.py

--- LabJournal/urls.py
+++ LabJournal/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('bdanswers/', include('bdanswers.urls')),
     path('postbox/', include('contact_form.urls')),
-    path('hike/', include('contact_form.urls')),
+    path('hike/', include('hike.urls')),
     # path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
