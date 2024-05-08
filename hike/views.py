1630a1261fcfff24cb3734c78b2bd865d1cb7369
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-05-08 11:20:49 +0300

    Update views.py

--- hike/views.py
+++ hike/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, CreateView
+
+from .forms import *
+from .models import *
+
+
+class HikeView(ListView):
+    """ Выводит список всех постов """
+    model = Blog
+    template_name = 'blog/blog.html'
+    context_object_name = 'objects'
+    ordering = ['-pk']
+    paginate_by = 6
+
+
+class HikeStrView(CreateView):
+    """ выводит отдельный пост """
+    model = Blog
+    template_name = 'blog/blogstr.html'
+    form_class = CommentCreationForm
+
+    def get_object(self, queryset=None):
+        return Blog.objects.get(pk=self.kwargs.get("pk"))
+
+    def get_context_data(self, **kwargs):
+        context = super(BlogStrView, self).get_context_data(**kwargs)
+        comments = Comments.objects.filter(forNote=self.kwargs['pk']).order_by("pk")
+        obj = Blog.objects.get(pk=self.kwargs.get("pk"))
+        context['form'] = CommentCreationForm()
+        context['comments'] = comments
+        context['obj'] = obj
+
+        return context
+
+    def form_valid(self, form):
+        order = form.save(commit=False)
+        order.forNote = Blog.objects.get(pk=self.kwargs['pk'])
+        order.save()
+        return super().form_valid(form)
