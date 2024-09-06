71bac5bf3952f4e4554da7df9a24c4729b3deab8
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-09-06 16:17:57 +0300

    Update settings.py

--- LabJournal/settings.py
+++ LabJournal/settings.py
@@ -156,8 +156,12 @@ USE_L10N = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATIC_URL = '/static/'
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [STATIC_DIR]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
