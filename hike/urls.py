1d40a2bfe088c969275329c8e608efce2e4a7cad
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-09-09 17:17:27 +0300

    Update urls.py

--- hike/urls.py
+++ hike/urls.py
@@ -6,6 +6,6 @@ from . constants import *
 
 urlpatterns = [
     path('', views.HikeAllListView.as_view(), name=URL + 'head'),
-    path('', views.BMAllListView.as_view(), name='bm'),
+    path('bm', views.BMAllListView.as_view(), name='bm'),
     path('<int:pk>/', views.HikeStrView.as_view(), name='hikestr'),
     ]
