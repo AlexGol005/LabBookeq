f224559d2db1c97a7eede45e333dba72333d012c
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-09-23 13:04:43 +0300

    Update forms.py

--- hike/forms.py
+++ hike/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from .models import *
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
+
 
 class CommentCreationForm(forms.ModelForm):
     """форма для  комментариев"""
