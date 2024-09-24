661164006b5dc1e6674bae5be4e33daa594683dc
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-09-24 14:35:32 +0300

    Update admin.py

--- hike/admin.py
+++ hike/admin.py
@@ -3,13 +3,21 @@ from .models import *
 
 
 
-admin.site.register(Hike)
-admin.site.register(Comments)
-admin.site.register(Bookmarks)
+
+
 
 @admin.register(Itbookmarks)
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('pk', 'type',)
     search_fields = ['pk']
-    
 
+@admin.register(Bookmarks)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'type',)
+    search_fields = ['pk']
+    
+@admin.register(Hike)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title',)
+
+    search_fields = ['pk', 'title']
