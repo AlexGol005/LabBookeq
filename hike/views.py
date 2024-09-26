94af8aba7b1a6c67cf7188bf26fe7109bab7766b
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-09-26 17:23:20 +0300

    Update views.py

--- hike/views.py
+++ hike/views.py
@@ -139,7 +139,7 @@ def hikefilterview(request, pk):
     # a = str(model.dates_try)[-2:]
     # b = str(model.dates_try)[-3:-1]
     if pk == 0:
-        objects = model.objects.filter(dates_try[-2:]=ar).order_by('-pk')
+        objects = model.objects.filter(dates_try[-2:]==ar).order_by('-pk')
     if pk == 1:   
         objects = model.objects.all()
     form = SearchForm() 
