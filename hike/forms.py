af803ecc27339b72095a96d9fcf429ec8732ff05
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-01 11:22:53 +0300

    Update forms.py

--- hike/forms.py
+++ hike/forms.py
@@ -20,7 +20,7 @@ class CommentCreationForm(forms.ModelForm):
 
 
 class SearchForm(forms.Form):
-    """форма для поиска по названию 1 полю модели"""
+    """форма для поиска по полям модели"""
     searchword = forms.CharField(label='Поиск', required=False,
                            help_text='слово для поиска с маленькой и большой буквы',
                            widget=forms.TextInput(attrs={'class': 'form-control'}))
