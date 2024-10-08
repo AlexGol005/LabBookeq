69bf43d73b95feaf6d07b657ce47af4a1c5572ce
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-08 15:36:56 +0300

    Update admin.py

--- hike/admin.py
+++ hike/admin.py
@@ -7,18 +7,15 @@ from import_export import fields
 from import_export.widgets import ForeignKeyWidget
 import tablib
 
-
+class KareliahistoryResource(resources.ModelResource):
+    class Meta:
+        model = Kareliahistory
 
         
-#class NoteAdmin(ImportExportActionModelAdmin):
-    #resource_class = ProductResource
-    #list_display = ('pk', 'text',)
-book_resource = resources.modelresource_factory(model=Kareliahistory)()
-dataset = tablib.Dataset(['', 'New book'], headers=['id', 'title'])
-result = book_resource.import_data(dataset, dry_run=True)
-print(result.has_errors())
-False
-result = book_resource.import_data(dataset, dry_run=False)
+class NoteAdmin(ImportExportActionModelAdmin):
+    resource_class = KareliahistoryResource
+    list_display = ('pk', 'text',)
+
 
 
 @admin.register(Itbookmarks)
