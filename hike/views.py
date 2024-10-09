8eca4313d48210aa55b79989b3d859e905c0aace
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-09 15:51:32 +0300

    Update views.py

--- hike/views.py
+++ hike/views.py
@@ -24,13 +24,13 @@ def index(request):
     categories = list(rs.index)
     values = list(rs.values)
 	
-	table_content = df.to_html(index=None)
+    table_content = df.to_html(index=None)
     table_content = table_content.replace("","")
     table_content = table_content.replace('class="dataframe"',"class='table table-striped'")
     table_content = table_content.replace('border="1"',"")
 	
     context = {"categories": categories, 'values': values, 'table_data':table_content}
-    return render(request, 'index.html', context=context)
+    return render(request, 'hike/example.html', context=context)
 
 class ExampleTemplateView(TemplateView):
     template_name = 'hike/example.html'
