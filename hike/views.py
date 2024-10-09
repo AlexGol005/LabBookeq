a642dd81dd91443abfef22a59d3d60859ba5c649
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-09 16:43:37 +0300

    Update views.py

--- hike/views.py
+++ hike/views.py
@@ -14,23 +14,7 @@ from django.views.generic import ListView, TemplateView, CreateView, UpdateView
 
 now = date.today()
 
-def index(request):
-    """ view function for sales app """
 
-    # read data                                                                                                  
-	
-    df = pd.read_csv("car_sales.csv")
-    rs = df.groupby("Engine size")["Sales in thousands"].agg("sum")
-    categories = list(rs.index)
-    values = list(rs.values)
-	
-    table_content = df.to_html(index=None)
-    table_content = table_content.replace("","")
-    table_content = table_content.replace('class="dataframe"',"class='table table-striped'")
-    table_content = table_content.replace('border="1"',"")
-	
-    context = {"categories": categories, 'values': values, 'table_data':table_content}
-    return render(request, 'hike/example.html', context=context)
 
 class ExampleTemplateView(TemplateView):
     template_name = 'hike/example.html'
