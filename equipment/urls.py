37a39d70493f69d4e7e9b95afc7c66a3a36e24d5
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-11 16:20:54 +0300

    Update urls.py

--- equipment/urls.py
+++ equipment/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('managerequipment/', views.ManagerEquipmentView.as_view(), name='managerequipment'),
     path('measureequipmentall/', views.MeasurEquipmentView.as_view(), name='measureequipmentall'),
     path('measureequipmentall/mustver/', views.SearchMustVerView.as_view(), name='mustver'),
     path('measureequipmentall/mustatt/', views.SearchMustAttView.as_view(), name='mustatt'),
