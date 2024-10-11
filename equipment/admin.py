1e5a7179fe78721356f798efb6373bef4d1dbed4
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-11 17:25:24 +0300

    Update admin.py

--- equipment/admin.py
+++ equipment/admin.py
@@ -21,7 +21,9 @@ admin.site.register(TestingEquipmentCharakters)
 admin.site.register(TestingEquipment)
 admin.site.register(Attestationequipment)
 admin.site.register(HelpingEquipmentCharakters)
-admin.site.register(HelpingEquipment)
+admin.site.register(HelpingEquipment) 
+admin.site.register(MeasurEquipmentReestr)
+
 
 
 
