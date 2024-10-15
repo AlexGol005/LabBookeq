568332eb74b8426836c8c3f92635f5cda5320859
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-15 13:36:49 +0300

    Update admin.py

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import *
 
+from import_export.admin import ImportExportActionModelAdmin
+from import_export import resources
+from import_export import fields
+from import_export.widgets import ForeignKeyWidget
+import tablib
+
 
 
 admin.site.register(Manufacturer)
