c85bb93a5b352c6297206e89b6d7e2bce7819157
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-15 13:46:06 +0300

    Update forms.py

--- equipment/forms.py
+++ equipment/forms.py
@@ -723,7 +723,7 @@ class HelpingEquipmentCharaktersCreateForm(forms.ModelForm):
 class MeasurEquipmentCreateForm(forms.ModelForm):
     """форма для внесения СИ"""
     charakters = forms.ModelChoiceField(label='Госреестр', required=False,
-                                        queryset=MeasurEquipmentCharakters.objects.all().order_by('name'),
+                                        queryset=MeasurEquipmentCharakters.objects.all(),
                                         widget=forms.Select(attrs={'class': 'form-control'}))
 
     class Meta:
