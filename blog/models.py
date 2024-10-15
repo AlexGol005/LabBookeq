2f492a0c430b9a0aa0a13d489a515f4384221c5f
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-15 16:44:44 +0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,21 @@
+from django.db import models
+from django.urls import reverse
 
+
+class Blog(models.Model):
+    date = models.DateField('Дата', auto_now_add=True)
+    title = models.CharField('Заголовок', max_length=10000)
+    metatitle = models.CharField('Метазаголовок страницы', max_length=10000, blank=True, null=True)
+    description = models.TextField('Метаописание страницы', blank=True, null=True)
+    keywords = models.TextField('Ключевые слова', blank=True, null=True)
+    text = models.TextField('Текст записи.')
+
+
+
+
+    def __str__(self):
+        return f' {self.date} , {self.title}'
+
+    class Meta:
+        verbose_name = 'О лабораторном оборудовании'
+        verbose_name_plural = 'О лабораторном оборудовании'
