1ce0d7686057b1c5ae9d97fe95a3faedd96a843e
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-18 16:08:23 +0300

    Update forms.py

--- equipment/forms.py
+++ equipment/forms.py
@@ -907,7 +907,7 @@ class VerificationRegForm(forms.ModelForm):
     # verificator = forms.ModelChoiceField(label='Организация-поверитель',
     #                                      queryset=Verificators.objects.all(),
     #                                      widget=forms.Select(attrs={'class': 'form-control'}))
-    verificator = AutoCompleteSelectField(label='Организация-поверитель', lookup_class = VerificatorsLookup, required=True,  help_text='Начните вводить название и появится список')
+    verificator = AutoCompleteSelectField('verificator_tag', label='Организация-поверитель', required=True,  help_text='Начните вводить название и появится список')
     place = forms.ChoiceField(label='Место поверки',
                               choices=CHOICESPLACE,
                               widget=forms.Select(attrs={'class': 'form-control'}))
