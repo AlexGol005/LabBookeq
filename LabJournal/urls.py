a418cb15b63d19ac0a22461a05d2c3e638919dc1
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-18 14:35:02 +0300

    Update urls.py

--- LabJournal/urls.py
+++ LabJournal/urls.py
@@ -6,7 +6,7 @@ from ajax_select import urls as ajax_select_urls
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^ajax_select/', include(ajax_select_urls)),
+    path("admin/lookups/", include(ajax_select_urls)),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('users.urls')),
