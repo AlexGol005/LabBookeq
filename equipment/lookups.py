75bec49d60d08941e908fe4ce0e8bf4174007944
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-18 16:41:20 +0300

    Update lookups.py

--- equipment/lookups.py
+++ equipment/lookups.py
@@ -11,8 +11,25 @@ class VerificatorsLookup(LookupChannel):
     def format_item_display(self, item):
         return u"<span class='verificator_tag'>%s</span>" % item.companyName
 
-    def format_match(self, item):
-        return u"<span class='verificator_tag'>%s</span>" % item.companyName
+        def get_query(self, q, request):
+        return moboListing.objects.filter(Q(moboList__icontains=q)).order_by('name')
+    def get_result(self, obj):
+        """ result is the simple text that is the completion of what the person typed """
+        return obj.companyName
+        
+    def format_match(self, obj):
+        """ (HTML) formatted item for display in the dropdown """
+        return self.format_item_display(obj)
+
+    def format_item_display(self, obj):
+        """ (HTML) formatted item for displaying item in the selected deck area """
+        return u"%s<div><i>%s</i></div>" % (escape(obj.companyName))
+
+    def get_objects(self, ids):
+        return model.objects.filter(pk__in=ids)
+
+    # def format_match(self, item):
+    #     return u"<span class='verificator_tag'>%s</span>" % item.companyName
 
 
 
