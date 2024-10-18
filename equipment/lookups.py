97a808246358e68164d1461ee59dfc66dcfa80e9
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-18 14:24:46 +0300

    Update lookups.py

--- equipment/lookups.py
+++ equipment/lookups.py
@@ -8,19 +8,15 @@ class VerificatorsLookup(LookupChannel):
     def get_query(self, q, request):
         return self.model.objects.filter(companyName__icontains=q).order_by('companyName')[:50]
 
-    def format_item_display(self, item):
-        return u"<span class='verificator_tag'>%s</span>" % item.companyName
+    def get_result(self,obj):
+        return obj.companyName
+        
+    def format_match(self,obj):
+        return self.format_item_display(obj)
+        
+    def format_item_display(self,obj):
+        return u"%s" % escape(obj.pk)
 
-    def format_item_display(self, item):
-        return """
-            <span class='product'>{}</span> 
-            <a 
-            id='change_id_order_product_{}'
-            class='related-widget-wrapper-link change-related' 
-            data-href-template='/admin/products/product/__fk__/change/?_to_field=id&_popup=1'
-            href='/admin/products/product/{}/change/?_to_field=id&_popup=1'
-            >
-            <img src="/static/admin/img/icon-changelink.svg" alt="Change">
-            </a>""".format(item.name, item.id, item.id)
+
 
 
