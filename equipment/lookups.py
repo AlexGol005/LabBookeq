b954bdc91d35a16ac1382125b13971722dfcf0e0
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-18 14:03:23 +0300

    Update lookups.py

--- equipment/lookups.py
+++ equipment/lookups.py
@@ -6,8 +6,8 @@ class VerificatorsLookup(LookupChannel):
     model = Verificators
 
     def get_query(self, q, request):
-        return self.model.objects.filter(companyName__icontains=q).order_by('name')[:50]
+        return self.model.objects.filter(companyName__icontains=q).order_by('companyName')[:50]
 
     def format_item_display(self, item):
-        return u"<span class='tag'>%s</span>" % item.name
+        return u"<span class='tag'>%s</span>" % item.companyName
       
