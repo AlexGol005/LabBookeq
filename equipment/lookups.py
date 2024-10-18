dda3d8d7c65cd96cd76d12b8033db37c2c6cfd9b
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-18 14:14:04 +0300

    Update lookups.py

--- equipment/lookups.py
+++ equipment/lookups.py
@@ -10,4 +10,17 @@ class VerificatorsLookup(LookupChannel):
 
     def format_item_display(self, item):
         return u"<span class='verificator_tag'>%s</span>" % item.companyName
-      
+
+    def format_item_display(self, item):
+        return """
+            <span class='product'>{}</span> 
+            <a 
+            id='change_id_order_product_{}'
+            class='related-widget-wrapper-link change-related' 
+            data-href-template='/admin/products/product/__fk__/change/?_to_field=id&_popup=1'
+            href='/admin/products/product/{}/change/?_to_field=id&_popup=1'
+            >
+            <img src="/static/admin/img/icon-changelink.svg" alt="Change">
+            </a>""".format(item.name, item.id, item.id)
+
+
