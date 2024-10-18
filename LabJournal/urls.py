da74dcb437441af3f6e11365dd0b989013d2efd2
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-18 11:18:10 +0300

    Update urls.py

--- LabJournal/urls.py
+++ LabJournal/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from ajax_select import urls as ajax_select_urls
 
 urlpatterns = [
+    path("admin/lookups/", include(ajax_select_urls)),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('users.urls')),
