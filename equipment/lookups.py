c0852a411897b0bb560e2a12fce89cba3c75fe7b
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-18 13:31:42 +0300

    Update lookups.py

--- equipment/lookups.py
+++ equipment/lookups.py
@@ -1,1 +1,13 @@
+from ajax_select import register, LookupChannel
+from .models import Verificationequipment
 
+@register('tags')
+class VerificationequipmentLookup(LookupChannel):
+    model = Verificationequipment
+
+    def get_query(self, q, request):
+        return self.model.objects.filter(name__icontains=q).order_by('name')[:50]
+
+    def format_item_display(self, item):
+        return u"<span class='tag'>%s</span>" % item.name
+      
