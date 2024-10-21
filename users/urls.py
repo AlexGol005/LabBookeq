581b315c657fa09e6f6a0de29463be04ed913075
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-21 17:21:54 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from users import views as UserView
 from django.contrib.auth import views as authViews
 
 urlpatterns = [
-    path('register/', UserView.register, name='reg'),
+    # path('register/', UserView.register, name='reg'),
     path('personal/', UserView.PersonalView.as_view(), name='personal'),
     path('login/', authViews.LoginView.as_view(template_name='users/user.html'), name='user'),
     path('exit/', authViews.LogoutView.as_view(template_name='users/exit.html'), name='exit'),
