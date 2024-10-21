ec92665af9402234ed7e6158c019535da9093ba5
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-21 17:28:54 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -6,10 +6,10 @@ from django.contrib.auth import views as authViews
 
 urlpatterns = [
     # path('register/', UserView.register, name='reg'),
-    path('personal/', UserView.PersonalView.as_view(), name='personal'),
+    path('Profile/', UserView.PersonalView.as_view(), name='profile'),
     path('login/', authViews.LoginView.as_view(template_name='users/user.html'), name='user'),
     path('exit/', authViews.LogoutView.as_view(template_name='users/exit.html'), name='exit'),
-    path('profile/', UserView.profile, name='profile'),
+    path('profileupdate/', UserView.profileupdate, name='profileupdate'),
     path('password_reset/',
     PasswordResetView.as_view(template_name='users/password_reset_form.html',
                                subject_template_name='users/password_reset_subject.html',
