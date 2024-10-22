6097f8ec64a198451dd15df3ba169b63facb1101
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-22 11:37:23 +0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -60,8 +60,8 @@ class ProfileView(LoginRequiredMixin, TemplateView):
                 context['USERTITLE'] = "Базовый пользователь"
         except:
             context['USER'] = False
-        employees = Employees.objects.filter(userid_userid=user.userid)
-        company = Company.objects.get(userid=user.userid)
+        employees = Employees.objects.filter(userid_userid=user.profile.userid)
+        company = Company.objects.get(userid=user.profile.userid)
         context['employees'] = employees
         context['company'] = company 
             
