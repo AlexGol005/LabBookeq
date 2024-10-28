979c23c5a201b638ee10bcdc3b783429b19bb50d
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-28 14:15:29 +0300

    Update views.py

--- administrator/views.py
+++ administrator/views.py
@@ -1,1 +1,11 @@
+from django.views.generic import TemplateView
+from django.db.models import Max
+from blog.models import *
 
+class RegstrView(TemplateView):
+    """ выводит отдельный пост """
+    model = Regstr
+    template_name = 'administrator/about_registration.html'
+
+    def get_object(self, queryset=None):
+        return Regstr.objects.get(date=Max('date'))
