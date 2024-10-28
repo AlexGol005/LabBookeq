9306b51e8145b148990e4106646d4e19d251ea83
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-28 14:45:53 +0300

    Update views.py

--- administrator/views.py
+++ administrator/views.py
@@ -6,6 +6,7 @@ class RegstrView(TemplateView):
     """ выводит отдельный пост """
     model = Regstr
     template_name = 'administrator/about_registration.html'
+    context_object_name = 'obj'
 
     def get_object(self, queryset=None):
         return Regstr.objects.get(date=Max('date'))
