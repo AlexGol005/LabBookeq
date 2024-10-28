c6b1f7f2a7aa87b0bcd7bafb3b30b601aa137a26
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-28 13:56:36 +0300

    Update models.py

--- administrator/models.py
+++ administrator/models.py
@@ -1,1 +1,16 @@
+from django.db import models
+from django.urls import reverse
 
+
+class Regstr(models.Model):
+    date = models.DateField('Дата', auto_now_add=True)
+    text = models.TextField('Текст записи')
+
+
+    def __str__(self):
+        return f' {self.date}'
+
+    class Meta:
+        verbose_name = 'О регистрации'
+        verbose_name_plural = 'О регистрации'
+      
