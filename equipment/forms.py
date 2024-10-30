e1a48264273ad5b84d4d04f7f1b6440b7f43e23f
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-30 15:28:24 +0300

    Update forms.py

--- equipment/forms.py
+++ equipment/forms.py
@@ -1224,10 +1224,8 @@ class RoomsUpdateForm(forms.ModelForm):
     """форма для создания обновления названия комнаты и оборудования"""
     def __init__(self, ruser, *args, **kwargs):
         super(RoomsUpdateForm, self).__init__(*args, **kwargs)
-        # self.fields['equipment1'].queryset = MeasurEquipment.objects.filter(pointer = ruser).filter(charakters__name__contains='Барометр') related_name='equipment1rooms'
-        # self.fields['equipment2'].queryset = MeasurEquipment.objects.filter(pointer = ruser).filter(charakters__name__contains='Гигрометр')
-        self.fields['equipment1'].queryset = MeasurEquipment.objects.filter(equipment_pointer = ruser)
-        self.fields['equipment2'].queryset = MeasurEquipment.objects.filter(equipment_pointer = ruser)
+        self.fields['equipment1'].queryset = MeasurEquipment.objects.filter(equipment__pointer = ruser).filter(charakters__name__contains='Барометр')
+        self.fields['equipment2'].queryset = MeasurEquipment.objects.filter(equipment__pointer = ruser).filter(charakters__name__contains='Гигрометр')
         
         roomnumber = forms.CharField(label='Название комнаты', widget=forms.TextInput(attrs={'class': 'form-control',}))
                              
