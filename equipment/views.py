9b1bacac34135dc466e410299e94f963a8982ac8
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-10-31 17:44:07 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -487,6 +487,10 @@ class MeteorologicalParametersRoomView(ListView):
             context['me'] = get_eq.equipments 
         except:
             context['me'] = 'добавьте средства измерения для комнаты и  первую запись о условиях микроклимата'
+                try:
+            context['rp'] = get_eq.person 
+        except:
+            context['rp'] = 'добавьте ответственного для комнаты и  первую запись о условиях микроклимата'
         context['title'] = Rooms.objects.get(id=self.kwargs['pk']).roomnumber
         context['titlepk'] = Rooms.objects.get(id=self.kwargs['pk']).pk
         context['form'] = DateForm()
