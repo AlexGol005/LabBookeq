697dde8c09251035a901fc429d4d918963159029
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-11-01 14:54:53 +0300

    Update urls.py

--- administrator/urls.py
+++ administrator/urls.py
@@ -6,5 +6,5 @@ from . import views
 
 urlpatterns = [
     path('', views.RegstrView.as_view(), name='reg'),
-    path('about/', views.About.as_view(), name='about'),
+    path('about/', views.AboutView.as_view(), name='about'),
     ]
