a044fa1d648f389e0de6cdab0c9e59e5b93869c1
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-11-01 14:52:27 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -5,11 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
-    path('attestationJ/', views.AttestationJView.as_view(), name='att'),
-    path('CertifiedValueJ', views.CertifiedValueJView.as_view(), name='cv'),
     path('equipment/', views.EquipmentView.as_view(), name='eq'),
-    path('attjreg/', views.attestationJRegView, name='attjreg'),
-    path('about/', views.About.as_view(), name='about'),
     path('contacts/', views.Contacts.as_view(), name='contacts'),
       ]
 
