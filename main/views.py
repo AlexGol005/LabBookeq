2466d90ede0b5df9fb13a6c133696404f4df3628
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-11-01 14:44:00 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
@@ -14,6 +15,7 @@ class About(View):
     def get(self, request):
         return render(request, 'main/about.html')
 
+
 class Contacts(View):
     """выводит страницу контакты"""
     def get(self, request):
@@ -26,22 +28,7 @@ class IndexView(View):
         return render(request, 'main/main.html')
 
 
-# @login_required
-class AttestationJView(View):
-    """выводит страницу Журналы измерений"""
-    def get(self, request):
-        objects = AttestationJ.objects.all()
-        return render(request, 'main/attestationJ.html', {'objects': objects})
-
-
-class CertifiedValueJView(View):
-    """выводит страницу Журналы результатов измерений"""
-    def get(self, request):
-        objects = ResultValueJ.objects.all()
-        return render(request, 'main/certifiedvalueJ.html', {'objects': objects})
-
-
-class EquipmentView(TemplateView):
+class EquipmentView(LoginRequiredMixin, TemplateView):
     """выводит страницу Инфраструктура лаборатории"""
     template_name = 'main/equipment.html'
 
@@ -56,26 +43,3 @@ class EquipmentView(TemplateView):
         except:
             context['USER'] = False
         return context
-
-
-@login_required
-def attestationJRegView(request):
-    """ выводит форму внесения журнала """
-    if request.method == "POST":
-        form = AttestationJForm(request.POST, request.FILES)
-        if form.is_valid():
-            order = form.save(commit=False)
-            order.performer = request.user
-            order.save()
-            messages.success(request,
-                             f'Заявка принята. Сообщите о заявке разработчику по a.golovkina@petroanalytica.ru')
-            return redirect('/attestationJ/')
-    else:
-        form = AttestationJForm()
-
-    return render(
-        request,
-        'main/registrationAtt.html',
-        {
-            'form': form
-        })
