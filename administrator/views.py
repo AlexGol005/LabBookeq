e2d77dad99c0d0e574aa52409cc2adc8689b8dd6
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-11-01 14:47:51 +0300

    Update views.py

--- administrator/views.py
+++ administrator/views.py
@@ -4,10 +4,19 @@ from .models import *
 
 class RegstrView(TemplateView):
     """ выводит отдельный пост """
-    template_name = 'administrator/about_registration.html'
+    template_name = 'administrator/tabletext.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['obj'] = Regstr.objects.latest('date')
         return context
 
+
+class AboutView(TemplateView):
+    """ выводит страницу о сайте """
+    template_name = 'administrator/tabletext.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['obj'] = About.objects.latest('date')
+        return context
