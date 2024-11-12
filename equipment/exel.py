eb41cce54312570ef6b1a5ce21ed24542ab29192
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-11-12 16:08:54 +0300

    Update exel.py

--- equipment/exel.py
+++ equipment/exel.py
@@ -4886,7 +4886,7 @@ def export_me_xls(request):
         filter(equipment__pointer=request.user.profile.userid).\
         exclude(equipment__status='С').\
         values_list(
-            'equipment__exnumber'[:5],
+            f'{'equipment__exnumber'}',
             'charakters__reestr',
             'charakters__name',
             'mod_type',
