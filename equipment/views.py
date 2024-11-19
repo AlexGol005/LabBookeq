acfa69c5f95f52f65ed4d4f4f8ba5cc201d62361
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-11-19 16:59:26 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -1720,7 +1720,7 @@ def ServiceEquipmentUFactUpdateView(request, str):
 def ServiceCreateView(request):
     queryset = Equipment.objects.filter(pointer=request.user.profile.userid)
     if request.method == 'POST':
-        year = request.POST('year')         
+        year = request.POST['year']         
         for i in queryset:
             ServiceEquipmentU.objects.get_or_create(equipment=i, year=year)
             ServiceEquipmentUFact.objects.get_or_create(equipment=i, year=year)
