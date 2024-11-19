b054beed20fa0fe9756e6eb994af150a78d3d366
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-11-19 16:46:31 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -1719,7 +1719,8 @@ def ServiceEquipmentUFactUpdateView(request, str):
 @login_required
 def ServiceCreateView(request):
     queryset = Equipment.objects.filter(pointer=request.user.profile.userid)
-    year = request.GET.get('year')   
+    year = request.GET.get('year') 
+    year = str(year)
     for i in queryset:
         ServiceEquipmentU.objects.get_or_create(equipment=i, year=year)
         ServiceEquipmentUFact.objects.get_or_create(equipment=i, year=year)
