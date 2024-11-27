bfde6078944b220ee788d51d14e2f08c6ed98357
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-11-27 16:05:53 +0300

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -155,9 +155,10 @@ class EmployeesUpdateForm(forms.ModelForm):
 
 class OrderformForm(forms.ModelForm):
     """форма для выбора формы заявки на поверку"""
-    orderform = forms.ChoiceField(label='Выберите форму заявки на поверку по поверителю, если нет нужной вам формы - обратитесь к администрации сайта', required=True,
-                             choices=ORDERFORMCHOISE,
-                             widget=forms.Select(attrs={'class': 'form-control'}))
+    # orderform = forms.ChoiceField(label='Выберите форму заявки на поверку по поверителю, если нет нужной вам формы - обратитесь к администрации сайта', required=True,
+    #                          choices=ORDERFORMCHOISE,
+    #                          widget=forms.Select(attrs={'class': 'form-control'}))
+    orderform = forms.CharField(label='ФИО', widget=forms.TextInput(attrs={'class': 'form-control',}))
                              
     class Meta:
         model = Company
