6b65b55bcc780fb5226024510206c3daa193be41
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-11-27 15:53:47 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -55,7 +55,7 @@ class OrderVerificationView(LoginRequiredMixin, ListView):
         return queryset
 
     def get_context_data(self, **kwargs):
-        company = Company.objects.filter(userid=self.request.user.profile.userid)
+        company = Company.objects.get(userid=self.request.user.profile.userid)
         a = company.orderform
         form = OrderformForm(initial={'orderform': company.orderform})
         return context
