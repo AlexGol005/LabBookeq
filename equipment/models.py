98727fcafea34afa25f99b17adb5221fd550b4ac
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-11-29 17:27:57 +0300

    Update models.py

--- equipment/models.py
+++ equipment/models.py
@@ -108,7 +108,7 @@ class Verificators(models.Model):
     head_name = models.CharField('Кому: имя лица организации-поверителя', max_length=100, default=None, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.companyName}'
+        return f'pk{self.companyName}-{self.companyName}'
 
     class Meta:
         verbose_name = 'Поверитель организация'
