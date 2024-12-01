458b31c4b9b3504f72eb428444595dfd27139b51
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-12-01 16:07:58 +0300

    Update forms.py

--- equipment/forms.py
+++ equipment/forms.py
@@ -36,8 +36,8 @@ class ActivAqqForm(forms.ModelForm):
         super(ActivAqqForm, self).__init__(*args, **kwargs)
         self.fields['qa'].queryset = Agreementverification.objects.filter(company=Company.objects.get(userid=ruser))
     
-    class Meta:
-        widgets = {'person':forms.Select(attrs={'class': 'form-control'}),}
+    # class Meta:
+    #     widgets = {'qa':forms.Select(attrs={'class': 'form-control'}),}
 
 
 # блок 1 - формы для поисков и распечатки этикеток
