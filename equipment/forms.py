48f21b27e002bcfd06e97632b28199d08e0c3804
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-12-01 16:14:53 +0300

    Update forms.py

--- equipment/forms.py
+++ equipment/forms.py
@@ -34,7 +34,8 @@ class ActivAqqForm(forms.Form):
     """форма для активации договора с поверителем"""
     def __init__(self, ruser, *args, **kwargs):
         super(ActivAqqForm, self).__init__(*args, **kwargs)
-        self.fields['verificator'].queryset = Agreementverification.objects.filter(company=Company.objects.get(userid=ruser))
+        # self.fields['verificator'].queryset = Agreementverification.objects.filter(company=Company.objects.get(userid=ruser))
+        self.fields['verificator'].queryset = Agreementverification.objects.all()
     
     class Meta:
         widgets = {'verificator':forms.Select(attrs={'class': 'form-control'}),}
