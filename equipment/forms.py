6aa37596759a1cd9f892358a8a5552ff7663882b
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-12-01 15:45:52 +0300

    Update forms.py

--- equipment/forms.py
+++ equipment/forms.py
@@ -33,10 +33,11 @@ from equipment.models import*
 class ActivAqqForm(forms.Form):
     """форма для активации договора с поверителем"""
     def __init__(self, ruser, *args, **kwargs):
-        super(PersonchangeForm, self).__init__(*args, **kwargs)
+        super(ActivAqqForm, self).__init__(*args, **kwargs)
         self.fields['qa'].queryset = Agreementverification.objects.filter(company=Company.objects.get(userid=ruser)
     class Meta:
         widgets = {'qa':forms.Select(attrs={'class': 'form-control'}),}
+
 
 # блок 1 - формы для поисков и распечатки этикеток
 
