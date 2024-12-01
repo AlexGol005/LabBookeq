68fc3a2a211964ac65f6f1e6167ee4aa25cb66a2
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-12-01 19:12:56 +0300

    Update forms.py

--- equipment/forms.py
+++ equipment/forms.py
@@ -35,7 +35,7 @@ class ActivAqqForm(forms.Form):
         ruser = self.ruser
         super(ActivAqqForm,self).__init__(*args,**kwargs)
 
-    queryset = Agreementverification.objects.filter(company=Company.objects.get(userid=ruser))
+        queryset = Agreementverification.objects.filter(company=Company.objects.get(userid=ruser))
         
     choiseagreement = forms.ModelChoiceField(queryset=queryset, label='Договор с поверителем', required=False, 
                            widget=forms.Select(attrs={'class': 'form-control'}))
