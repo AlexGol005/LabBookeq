26f3c82cf00a2fc9707d65d4200a8dd66d397f54
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2024-12-26 15:44:41 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -52,7 +52,7 @@ class OrderVerificationView(LoginRequiredMixin, View):
 
     def get(self, request):
         ruser=request.user.profile.userid
-        form =  Activaqqchange(ruser, initial={'ruser': ruser,})
+        form =  ActivaqqchangeForm(ruser, initial={'ruser': ruser,})
         list = Equipment.objects.filter(pointer=self.request.user.profile.userid) 
         context = {
             'form': form,
@@ -65,7 +65,7 @@ class OrderVerificationView(LoginRequiredMixin, View):
 
     def post(self, request, *args, **kwargs):
         ruser=request.user.profile.userid
-        form = Activaqqchange(ruser, request.POST)
+        form = ActivaqqchangeForm(ruser, request.POST)
         if request.user.has_perm('equipment.add_equipment') or request.user.is_superuser:
             if form.is_valid():
                 n = self.request.POST.get('choiseagreement')
