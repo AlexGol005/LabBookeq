1360297da02b36201275c3eeed62be323735c366
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-04 17:36:25 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -56,11 +56,8 @@ class OrderVerificationView(LoginRequiredMixin, View):
     def get(self, request, str):
         ruser=request.user.profile.userid
         form = ActivaqqchangeForm(ruser, instance=Activeveraqq.objects.get(pointer=ruser), initial={'ruser': ruser,})
-        str=str
-        if str == 0:
-            list = Equipment.objects.filter(pointer=self.request.user.profile.userid) 
-        elif str == 4:
-            list = Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(pk=10)
+
+        list = Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(pk=str)
         
         context = {
             'form': form,
