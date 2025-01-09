6aca9cc598a4bbebb5d86eb34252220a03ef124e
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-09 17:20:57 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -61,7 +61,7 @@ class OrderVerificationView(LoginRequiredMixin, View):
         if i=='0':
             list = Equipment.objects.filter(pointer=self.request.user.profile.userid)
         if i=='4':
-            list = Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(testingequipment__newhaveorder=True) 
+            list = Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(testingequipment__newhaveorder=True) & Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(measurequipment__newhaveorder=True)
         else:
             list = Equipment.objects.filter(pointer=self.request.user.profile.userid)
         
