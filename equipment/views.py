c2ed3542958ac7b25dfe0b072336e03284f2482f
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-10 17:13:11 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -55,7 +55,10 @@ class OrderVerificationView(LoginRequiredMixin, View):
     
     def get(self, request, str):
         ruser=request.user.profile.userid
-        serdate = request.GET.get('date')
+        try:
+            serdate = request.GET.get('date')
+        except:
+            serdate = now
         form = ActivaqqchangeForm(ruser, instance=Activeveraqq.objects.get(pointer=ruser), initial={'ruser': ruser,})
         dateform = DateForm()
         i=str
