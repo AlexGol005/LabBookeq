cdb7075e8d1905f48670f6be8da4a54cc7178f06
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-14 18:21:21 +0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -94,8 +94,9 @@ class CompanyProfileView(LoginRequiredMixin, TemplateView):
 @login_required
 def CompanyUpdateView(request):
     """выводит форму для обновления данных о компании"""
-    ruser = request.user.profile.userid
+    ruser = request.user
     if ruser.has_perm('equipment.add_equipment') or ruser.is_superuser:
+        ruser = request.user.profile.userid
         if request.method == "POST":
             form = CompanyCreateForm(request.POST, instance=Company.objects.get(userid=ruser))
             if form.is_valid():
