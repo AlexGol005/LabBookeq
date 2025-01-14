56c5c590097cbb22d14c4e08e0d3afb884a2156b
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-14 15:01:55 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -65,14 +65,14 @@ class OrderVerificationView(LoginRequiredMixin, View):
             list = Equipment.objects.filter(pointer=self.request.user.profile.userid)
         if i=='4':
             list = Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(measurequipment__newhaveorder=True) | Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(testingequipment__newhaveorder=True)
-        # if i=='1':
-        #     list = Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(measurequipment__newdatedead_date__lte=serdate) | Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(testingequipment__newdatedead_date__lte=serdate)
-        # if i=='2':
-        #     list = Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(measurequipment__newdateorder_date__lte=serdate) | Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(testingequipment__newdateorder_date__lte=serdate)
-        # if i=='3':
-        #     list = Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(measurequipment__newdateordernew_date__lte=serdate) | Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(testingequipment__newdateordernew_date__lte=serdate)
-        # else:
-            # list = Equipment.objects.filter(pointer=self.request.user.profile.userid)
+        if i=='1':
+            list = Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(measurequipment__newdatedead_date__lte=serdate) | Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(testingequipment__newdatedead_date__lte=serdate)
+        if i=='2':
+            list = Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(measurequipment__newdateorder_date__lte=serdate) | Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(testingequipment__newdateorder_date__lte=serdate)
+        if i=='3':
+            list = Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(measurequipment__newdateordernew_date__lte=serdate) | Equipment.objects.filter(pointer=self.request.user.profile.userid).filter(testingequipment__newdateordernew_date__lte=serdate)
+        else:
+            list = Equipment.objects.filter(pointer=self.request.user.profile.userid)
    
         context = {
             'form': form,
