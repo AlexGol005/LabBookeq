8ae9c5933b420308e8c321f00926cc6ca780e6e1
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-14 16:49:10 +0300

    Update exel_agreements.py

--- equipment/exel_agreements.py
+++ equipment/exel_agreements.py
@@ -29,3 +29,49 @@ from users.models import Profile, Company
 
 URL = 'equipment'
 now = date.today()
+from exel import get_affirmation, get_author
+
+
+
+
+# Стили
+# обкновенные границы ячеек
+b1 = Borders()
+b1.left = 1
+b1.right = 1
+b1.top = 1
+b1.bottom = 1
+
+# a1 выравнивание по центру по горизонтали и по вертикали, обтекание wrap тип 1
+a1 = Alignment()
+a1.horz = Alignment.HORZ_CENTER
+a1.vert = Alignment.VERT_CENTER
+a1.wrap = 1
+
+
+# style_plain_border обычные ячейки, с границами ячеек
+style_plain_border = xlwt.XFStyle()
+style_plain.font.name = 'Times New Roman'
+style_plain.borders = b1
+style_plain.alignment = a1
+style_plain.font.height = 20 * size
+
+
+def export_orderverification_xls(request, object_ids):
+    '''представление для выгрузки списка на поверку'''
+    company = Company.objects.get(userid=request.user.profile.userid)
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = f'attachment; filename="1.xls"'
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('1', cell_overwrite_ok=True)
+    q = object_ids[17:-3].split("', '")    
+    note = Equipment.objects.filter(id__in=q)
+    rows = note.values_list(
+        'pk', )
+    row_num = 1
+    for row in rows:
+        row_num += 1
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, row[col_num], style_plain_border)
+    wb.save(response)
+    return response
