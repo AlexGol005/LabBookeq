3a52d398e5361119fa75dca8624f8e254230d95e
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-15 16:25:28 +0300

    Update urls.py

--- equipment/urls.py
+++ equipment/urls.py
@@ -94,6 +94,7 @@ urlpatterns = [
     path(r'^export_orderverification/xls/$/<path:object_ids>/', exel_agreements.export_orderverification_xls, name='export_orderverification_xls'),
     path(r'^export_orderverification_14/xls/$/<path:object_ids>/', exel_agreements.export_orderverification_14_xls, name='export_orderverification_14_xls'),
     path(r'^export_orderverification_1/xls/$/<path:object_ids>/', exel_agreements.export_orderverification_1_xls, name='export_orderverification_1_xls'),
+    path(r'^export_orderverification_9/xls/$/<path:object_ids>/', exel_agreements.export_orderverification_9_xls, name='export_orderverification_9_xls'),
     path('companyverreg/', views.VerificatorRegView.as_view(), name='companyverreg'),
     path('agreementcompanyverreg/', views.AgreementVerificatorRegView.as_view(), name='agreementcompanyverreg'),  
     path('agreementcompanyverupdate/<str:str>/', views.AgreementVerificatorUpdateView, name='agreementcompanyverupdate'),
