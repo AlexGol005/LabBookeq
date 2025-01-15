6ccc4dab10d0993d2cb9b151a24cdc21648f0780
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-15 14:54:34 +0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -102,11 +102,12 @@ def CompanyUpdateView(request):
             form = CompanyCreateForm(request.POST, instance=Company.objects.get(userid=ruser))
             if form.is_valid():
                 order = form.save(commit=False)
+                order.save() 
                 n = Agreementverification.objects.get_or_create(active=True, company=Company.objects.get(userid=ruser), verificator=Verificators.objects.get(pk=14), pointer=ruser)
                 n.save()
-                n1=Activeveraqq.objects.get_or_create(aqq=n, company=Company.objects.get(userid=ruser))
-                n1.save()
-                order.save()                
+                # n1=Activeveraqq.objects.get_or_create(aqq=n, company=Company.objects.get(userid=ruser))
+                # n1.save()
+                               
                 return redirect('companyprofile')
         else:
             form = CompanyCreateForm(instance=Company.objects.get(userid=ruser))
