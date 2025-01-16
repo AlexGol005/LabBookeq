f104d46a9d71ce5cc21eaaa3b705485bfb35233d
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-16 09:21:58 +0300

    Update exel_agreements.py

--- equipment/exel_agreements.py
+++ equipment/exel_agreements.py
@@ -51,13 +51,31 @@ a1.horz = Alignment.HORZ_CENTER
 a1.vert = Alignment.VERT_CENTER
 a1.wrap = 1
 
+# a2 выравнивание по центру по вертикали и справа по горизонтали, обтекание wrap тип 1
+a1 = Alignment()
+a2.horz = Alignment.HORZ_RIGHT
+a2.vert = Alignment.VERT_CENTER
+a2.wrap = 1
 
-# style_plain_border обычные ячейки, с границами ячеек
+
+# style_plain_border обычные ячейки, с границами 
 style_plain_border = xlwt.XFStyle()
 style_plain_border.font.name = 'Times New Roman'
 style_plain_border.borders = b1
 style_plain_border.alignment = a1
 style_plain_border.font.height = 20 * size
+
+# style_plain_noborder обычные ячейки, без границ
+style_plain_noborder = xlwt.XFStyle()
+style_plain_noborder.font.name = 'Times New Roman'
+style_plain_noborder.alignment = a1
+style_plain_noborder.font.height = 20 * size
+
+# style_right_noborder обычные ячейки, без границ, выравнивание по правому краю
+style_plain_noborder = xlwt.XFStyle()
+style_plain_noborder.font.name = 'Times New Roman'
+style_plain_noborder.alignment = a2
+style_plain_noborder.font.height = 20 * size
 
 
 def export_orderverification_template_xls(object_ids):
@@ -82,6 +100,9 @@ def export_orderverification_xls(request, object_ids):
     response['Content-Disposition'] = f'attachment; filename="{exelnumber}.xls"'
     wb = xlwt.Workbook(encoding='utf-8')
     ws = wb.add_sheet(f'{exelnumber}', cell_overwrite_ok=True)
+    ws.header_str = b' '
+    ws.footer_str = b' '
+    
     q = object_ids[17:-3].split("', '")
     try:
         note = Equipment.objects.filter(id__in=q)
@@ -89,6 +110,22 @@ def export_orderverification_xls(request, object_ids):
         note = Equipment.objects.filter(id=1)
     rows = note.values_list(
         'pk', )
+    # конец стандартной шапки
+    
+    # ширина колонок и их количество
+    len_sheet = 11
+    ws.col(0).width = 500
+    ws.col(1).width = 500
+    ws.col(2).width = 500
+    ws.col(3).width = 3000
+    ws.col(4).width = 2000
+    ws.col(5).width = 500
+    ws.col(6).width = 500
+    ws.col(7).width = 500
+    ws.col(8).width = 2000
+    ws.col(9).width = 1500
+    ws.col(10).width = 1500
+    ws.col(11).width = 500
     
     row_num = 1
     for row in rows:
@@ -113,6 +150,9 @@ def export_orderverification_14_xls(request, object_ids):
     response['Content-Disposition'] = f'attachment; filename="{exelnumber}.xls"'
     wb = xlwt.Workbook(encoding='utf-8')
     ws = wb.add_sheet(f'{exelnumber}', cell_overwrite_ok=True)
+    ws.header_str = b' '
+    ws.footer_str = b' '
+    
     q = object_ids[17:-3].split("', '")
     try:
         note = Equipment.objects.filter(id__in=q)
@@ -120,6 +160,22 @@ def export_orderverification_14_xls(request, object_ids):
         note = Equipment.objects.filter(id=1)
     rows = note.values_list(
         'pk', )
+    # конец стандартной шапки
+    
+    # ширина колонок и их количество
+    len_sheet = 11
+    ws.col(0).width = 500
+    ws.col(1).width = 500
+    ws.col(2).width = 500
+    ws.col(3).width = 3000
+    ws.col(4).width = 2000
+    ws.col(5).width = 500
+    ws.col(6).width = 500
+    ws.col(7).width = 500
+    ws.col(8).width = 2000
+    ws.col(9).width = 1500
+    ws.col(10).width = 1500
+    ws.col(11).width = 500
     
     row_num = 1
     for row in rows:
@@ -144,6 +200,9 @@ def export_orderverification_1_xls(request, object_ids):
     response['Content-Disposition'] = f'attachment; filename="{exelnumber}.xls"'
     wb = xlwt.Workbook(encoding='utf-8')
     ws = wb.add_sheet(f'{exelnumber}', cell_overwrite_ok=True)
+    ws.header_str = b' '
+    ws.footer_str = b' '
+    
     q = object_ids[17:-3].split("', '")
     try:
         note = Equipment.objects.filter(id__in=q)
@@ -151,8 +210,79 @@ def export_orderverification_1_xls(request, object_ids):
         note = Equipment.objects.filter(id=1)
     rows = note.values_list(
         'pk', )
+    # конец стандартной шапки
+    
+    # ширина колонок и их количество
+    len_sheet = 11
+    ws.col(0).width = 500
+    ws.col(1).width = 500
+    ws.col(2).width = 500
+    ws.col(3).width = 3000
+    ws.col(4).width = 2000
+    ws.col(5).width = 500
+    ws.col(6).width = 500
+    ws.col(7).width = 500
+    ws.col(8).width = 2000
+    ws.col(9).width = 1500
+    ws.col(10).width = 1500
+    ws.col(11).width = 500
+
+    # переменные
+    verificator_head_position = a.aqq.verificator.head_position
+    verificator_companyName  = a.aqq.verificator.companyName 
+    verificator_head_name = a.aqq.verificator.head_name 
     
     row_num = 1
+    columns = [f'Заявка'
+    ]
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], style_plain_noborder)
+        ws.merge(row_num, row_num, 0, len_sheet)
+
+    row_num += 1
+    columns = [f'на выполнение работ (оказание услуг) по поверке (калибровке) СИ, аттестации ИО и иных работ (услуг) в области обеспечения единства'
+    ]
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], style_plain_noborder)
+        ws.merge(row_num, row_num, 0, len_sheet
+
+    row_num += 1
+    columns = [f'измерений'
+    ]
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], style_plain_noborder)
+        ws.merge(row_num, row_num, 0, len_sheet)
+
+    row_num += 1
+    columns = [f'измерений'
+    ]
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], style_plain_noborder)
+        ws.merge(row_num, row_num, 0, len_sheet)
+
+    row_num += 1
+    columns = [f'{verificator_head_position}'
+    ]
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], style_right_noborder)
+        ws.merge(row_num, row_num, len_sheet-3, len_sheet)
+
+    row_num += 1
+    columns = [f'{verificator_companyName}'
+    ]
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], style_right_noborder)
+        ws.merge(row_num, row_num, len_sheet-3, len_sheet)
+
+    row_num += 1
+    columns = [f'{verificator_head_name}'
+    ]
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], style_right_noborder)
+        ws.merge(row_num, row_num, len_sheet-3, len_sheet)
+
+
+    
     for row in rows:
         row_num += 1
         for col_num in range(len(row)):
@@ -174,6 +304,9 @@ def export_orderverification_9_xls(request, object_ids):
     response['Content-Disposition'] = f'attachment; filename="{exelnumber}.xls"'
     wb = xlwt.Workbook(encoding='utf-8')
     ws = wb.add_sheet(f'{exelnumber}', cell_overwrite_ok=True)
+    ws.header_str = b' '
+    ws.footer_str = b' '
+    
     q = object_ids[17:-3].split("', '")
     try:
         note = Equipment.objects.filter(id__in=q)
@@ -181,6 +314,22 @@ def export_orderverification_9_xls(request, object_ids):
         note = Equipment.objects.filter(id=1)
     rows = note.values_list(
         'pk', )
+    # конец стандартной шапки
+    
+    # ширина колонок и их количество
+    len_sheet = 11
+    ws.col(0).width = 500
+    ws.col(1).width = 500
+    ws.col(2).width = 500
+    ws.col(3).width = 3000
+    ws.col(4).width = 2000
+    ws.col(5).width = 500
+    ws.col(6).width = 500
+    ws.col(7).width = 500
+    ws.col(8).width = 2000
+    ws.col(9).width = 1500
+    ws.col(10).width = 1500
+    ws.col(11).width = 500
 
     row_num = 1
     for row in rows:
