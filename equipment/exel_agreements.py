99aa60ea0aee198fe510853fa64d568a51376b23
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-17 14:56:20 +0300

    Update exel_agreements.py

--- equipment/exel_agreements.py
+++ equipment/exel_agreements.py
@@ -82,12 +82,12 @@ style_plain_border.alignment = acc
 style_plain_border.font.height = 20 * size
 
 # style_plain_border обычные ячейки, с границами, повернут текст на 90 градусов
-style_plain_border_90 = xlwt.XFStyle()
+style_plain_border_90 = xlwt.XFStyle('align: rotation 90')
 style_plain_border_90.font.name = 'Times New Roman'
 style_plain_border_90.borders = b1
 # style_plain_border_90.alignment = acc90
 style_plain_border_90.font.height = 20 * size
-style_plain_border_90.easyxf('align: rotation 90')
+
 
 # style_plain_noborder обычные ячейки, без границ
 style_plain_noborder = xlwt.XFStyle()
