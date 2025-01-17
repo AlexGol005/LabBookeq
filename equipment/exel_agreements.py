631fc29c168edb94b8b39e5021a10d392a0952aa
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-17 17:21:08 +0300

    Update exel_agreements.py

--- equipment/exel_agreements.py
+++ equipment/exel_agreements.py
@@ -701,7 +701,7 @@ def export_orderverification_9_xls(request, object_ids):
          ws.merge(row_num, row_num+1, col_num+1, col_num+1, st90)
     for col_num in range(4, 6):
          ws.write(row_num, col_num+1, table_headers[col_num], style_plain_border)
-         ws.merge(row_num, row_num, col_num+1, col_num+1)
+         ws.merge(row_num, row_num, col_num+1, col_num+2)
 
         
 
