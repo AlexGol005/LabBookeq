a5b617f1a01a00f6cc94b2ffefd479e8246bfd63
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-23 16:27:46 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -2044,9 +2044,9 @@ class ServiceSearchResultView(LoginRequiredMixin, ListView):
 @login_required
 def ServiceCreateView(request):
     """формирует график ТОИР на указанный год """
-    queryset = Equipment.objects.filter(pointer=request.user.profile.userid)
     if request.method == 'GET':
         year = request.GET.get('date')
+        queryset = Equipment.objects.filter(pointer=request.user.profile.userid).filter(yearintoservice__gte=year)
     for i in queryset:
         ServiceEquipmentU.objects.get_or_create(equipment=i, year=year)
     return redirect('service')
