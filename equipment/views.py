409f23e5ee221ae83a20fd1b47066d982722ff13
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-01-28 17:54:10 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -1997,7 +1997,8 @@ class ServiceStrView(LoginRequiredMixin, View):
     def get(self, request, str):
         obj = get_object_or_404(ServiceEquipmentU, pk=str)
         obj2 = get_object_or_404(ServiceEquipmentUFact, pk_pointer=str)
-        year = now.year
+        year = self.request.GET['year']
+        # year = now.year
         context = {
             'obj': obj, 'obj2': obj2, 'year': year,
             }
