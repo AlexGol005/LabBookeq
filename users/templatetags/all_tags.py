94140acb806aea67ff73381f846e9ce4c2bcb459
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-06 11:58:01 +0300

    Update all_tags.py

--- users/templatetags/all_tags.py
+++ users/templatetags/all_tags.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 
 register = template.Library()
 
+# name 'request' is not defined!!!
 @register.simple_tag()
-def get_USER_HAVE_RIGHTS():
+def get_USER_HAVE_RIGHTS(request):
     user = User.objects.get(username=request.user)
     USER_HAVE_RIGHTS = user
  
