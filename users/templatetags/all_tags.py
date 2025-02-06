6b61b6321c9fe6db9beebb0eaa294d7013008183
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-06 11:51:13 +0300

    Update all_tags.py

--- users/templatetags/all_tags.py
+++ users/templatetags/all_tags.py
@@ -8,7 +8,7 @@ register = template.Library()
 def get_USER_HAVE_RIGHTS():
     try:
         user = User.objects.get(username=request.user)
-        USER_HAVE_RIGHTS = False
+        USER_HAVE_RIGHTS = 1
     except:
-        USER_HAVE_RIGHTS = False    
+        USER_HAVE_RIGHTS = 2   
     return  USER_HAVE_RIGHTS  
