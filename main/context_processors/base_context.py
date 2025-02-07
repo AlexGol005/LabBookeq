f979a3d2af3875946d81b09a2a27def24071dfc0
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-07 13:22:31 +0300

    Update base_context.py

--- main/context_processors/base_context.py
+++ main/context_processors/base_context.py
@@ -18,7 +18,21 @@ def last_ad(request):
 
 
 def USER_HAVE_RIGHTS(request):
-    '''что это?'''
+    '''это тег для шаблонов, {% if USER_HAVE_RIGHTS %}  чтобы показывать части шаблона только продвинутым пользователям и cкрывать от базовых пользователей '''
+    try:
+        user = User.objects.get(username=request.user)
+        if request.user.has_perm('equipment.add_equipment') or request.user.is_superuser:
+            USER_HAVE_RIGHTS = True
+        else:
+            USER_HAVE_RIGHTS = False
+    except:
+        USER_HAVE_RIGHTS = False
+    return {
+        'USER_HAVE_RIGHTS': USER_HAVE_RIGHTS
+    }
+
+def USER_AND_OWNER_HAVE_RIGHTS(request):
+    '''здесь будет тег для шаблонов, {% if USER_AND_OWNER_HAVE_RIGHTS %}  чтобы показывать части шаблона только продвинутым пользователям, владельцам приборов,  и cкрывать от базовых пользователей '''
     try:
         user = User.objects.get(username=request.user)
         if request.user.has_perm('equipment.add_equipment') or request.user.is_superuser:
