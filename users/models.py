034f8e578c86df4237fe552f4f14b4ea084a734e
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-07 15:53:42 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
     user_email = models.CharField('email', max_length=40, null=True, blank=True)
     user_phone = models.CharField('телефон', max_length=40, null=True, blank=True)
     userposition = models.CharField('Должность', max_length=50, null=True, blank=True)
-    userid = models.CharField('Идентификатор организации (20 случайных цифр и латинских букв)', max_length=50, null=True, blank=True)
+    userid = models.CharField('Идентификатор организации (20 случайных цифр и латинских букв)', max_length=50, default = 1, null=True, blank=True)
     img = models.ImageField('Фото сотрудника', default='user_images/default.png', upload_to='user_images')
 
 
