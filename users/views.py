e64a14164dade07dd228308c0509ad431a698900
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-07 15:21:34 +0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -164,9 +164,9 @@ def Employeereg(request):
                 username = form.cleaned_data.get('username')
                 messages.success(request, f'Пользовать {username} был успешно создан!')
                 return redirect('employees')
-            else:
-                messages.success(request, 'Раздел доступен только продвинутому пользователю')
-                return redirect('employees')
+        else:
+            messages.success(request, 'Раздел доступен только продвинутому пользователю')
+            return redirect('employees')
     else:
         form = UserRegisterForm()
         data =         {
