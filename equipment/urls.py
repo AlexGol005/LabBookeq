0b0c3d74a9aa252c01fa603493d478ccf8fb5f7c
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-16 17:55:04 +0300

    Update urls.py

--- equipment/urls.py
+++ equipment/urls.py
@@ -5,6 +5,8 @@ from . import exel
 from . import exel_agreements
 
 urlpatterns = [
+    path(r'^hecharakters-autocomplete/$', views_autocomplete_list.HelpingEquipmentCharaktersAutocomplete.as_view(), name='hecharakters-autocomplete',),
+    path(r'^techarakters-autocomplete/$', views_autocomplete_list.TestingEquipmentCharaktersAutocomplete.as_view(), name='techarakters-autocomplete',),
     path(r'^mecharakters-autocomplete/$', views_autocomplete_list.MeasurEquipmentCharaktersAutocomplete.as_view(), name='mecharakters-autocomplete',),
     path(r'^manufacturer-autocomplete/$', views_autocomplete_list.ManufacturerAutocomplete.as_view(), name='manufacturer-autocomplete',),
     path(r'^verificators-autocomplete/$', views_autocomplete_list.VerificatorsAutocomplete.as_view(), name='verificators-autocomplete',),
