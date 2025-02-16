9d5b0233863d6e5d61a7c634102da4b2568050eb
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-16 15:26:12 +0300

    Update views_autocomplete_list.py

--- equipment/views_autocomplete_list.py
+++ equipment/views_autocomplete_list.py
@@ -12,3 +12,14 @@ class VerificatorsAutocomplete(autocomplete.Select2QuerySetView):
             qs = qs.filter(query)
         return qs
 
+
+class ManufacturerAutocomplete(autocomplete.Select2QuerySetView):
+    """выводит список производителей для формы"""
+    """path(r'^manufacturer-autocomplete/$', views_autocomplete_list.ManufacturerAutocomplete.as_view(), name='manufacturer-autocomplete',),"""
+    def get_queryset(self):
+        qs = Manufacturer.objects.all()
+        if self.q:
+            query = Q(companyName__contains=self.q.lower()) | Q(companyName__contains=self.q.upper())
+            qs = qs.filter(query)
+        return qs
+
