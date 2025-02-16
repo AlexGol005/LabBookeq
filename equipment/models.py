717e32f39c77c81c1ea7678261813030f1236a40
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-16 21:16:37 +0300

    Update models.py

--- equipment/models.py
+++ equipment/models.py
@@ -96,7 +96,7 @@ class Manufacturer(models.Model):
         verbose_name = 'Производитель'
         verbose_name_plural = 'Производители'
 
-from  LabJournal.middleware  import *
+from  LabJournal.middleware.current_user  import *
 class Verificators(models.Model):
     """Компании поверители оборудования"""
     companyName = models.CharField('Поверитель', max_length=100, unique=True)
