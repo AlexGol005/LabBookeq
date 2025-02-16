4797343f15e16830c7441fcc1e7bcf83492c39fc
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-16 22:43:28 +0300

    Update settings.py

--- LabJournal/settings.py
+++ LabJournal/settings.py
@@ -82,6 +82,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'users.middleware.CurrentUserMiddleware',
+    'django_currentuser.middleware.ThreadLocalUserMiddleware',
 ]
 
 
