6fc0431a07fc9441dd0aeee92ecb75045dc9f904
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-16 11:51:05 +0300

    Update forms.py

--- equipment/forms.py
+++ equipment/forms.py
@@ -894,7 +894,7 @@ class AgreementVerificatorsCreationForm(forms.ModelForm):
                  ]
 
         widgets = {
-            "verificator" : autocomplete.ModelSelect2(url=reverse_lazy('verificators-autocomplete'), attrs={'class' : 'form-control', 'data-placeholder' : "Name"}),
+            "verificator" : autocomplete.ModelSelect2(url=reverse_lazy('verificators-autocomplete'), attrs={'class' : 'form-control', 'data-placeholder' : "Name"})
 
                
 
