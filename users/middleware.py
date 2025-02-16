39490af4e3c47bb8e462e84e0595d6281bf6916a
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-16 22:37:39 +0300

    Update middleware.py

--- users/middleware.py
+++ users/middleware.py
@@ -3,9 +3,6 @@ from django.utils.deprecation import MiddlewareMixin
 
 class CurrentUserMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        # Do something with request.user
-        if request.user.is_authenticated:
-            # print(f"Current user: {request.user.username}")
         return request.user
 
   
