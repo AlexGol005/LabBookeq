5dc2964dfdb7aea7934a14f8daf75847484dfcd3
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-16 20:04:13 +0300

    Update current_user.py

--- equipment/middleware/current_user.py
+++ equipment/middleware/current_user.py
@@ -4,12 +4,12 @@ _user = local()
 
 class CurrentUserMiddleware(object):
 
-def __init__(self, get_response):
-    self.get_response = get_response
-
-def __call__(self, request):
-    _user.value = request.user
-    return self.get_response(request)
-
-def get_current_user():
-    return _user.value
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        _user.value = request.user
+        return self.get_response(request)
+    
+    def get_current_user():
+        return _user.value
