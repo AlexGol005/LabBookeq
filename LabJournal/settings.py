6d5323afe91979a9d0fd8f92434ea44ffc3981d6
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-16 21:43:59 +0300

    Update settings.py

--- LabJournal/settings.py
+++ LabJournal/settings.py
@@ -86,9 +86,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'crum.CurrentRequestUserMiddleware',
 ]
 
-MIDDLEWARE += ('crum.CurrentRequestUserMiddleware',)
+
 
 ROOT_URLCONF = 'LabJournal.urls'
 
