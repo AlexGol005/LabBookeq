ec3ae60d5d4342b953b0f757ef735ed2915f7492
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-21 20:29:19 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -2566,6 +2566,6 @@ def EquipmentKategoryUpdate(request, str):
         return render(request, 'equipment/Eindividuality.html', data)
     else:
         messages.success(request, f' Раздел доступен только продвинутому пользователю')
-        return redirect('equipmentlist')
+        return redirect(f'"equipmentkategoryupdate/{str}/"')
 
 
