972f16294e44959927c6ad42543df80abb917ceb
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-25 21:48:14 +0300

    Update signals.py

--- equipment/signals.py
+++ equipment/signals.py
@@ -14,5 +14,12 @@ def lead_post_init(sender, instance, **kwargs):
 @receiver(post_save, sender=Equipment, dispatch_uid="update_stock_count")
 def criar_slug(sender, instance, created,**kwargs):
   if instance._previous_status != instance.status:
-    CommentsEquipment.objects.create(forNote = instance, note = 'поменяли статус', date = now)
+    if instance.status == 'Э':
+      CommentsEquipment.objects.create(forNote = instance, note = 'Оборудование введено в эксплуатацию', date = now, type = 'добавлена автоматически при изменении статуса')
+    if instance.status == 'РЕ':
+      CommentsEquipment.objects.create(forNote = instance, note = 'Отправлено в ремонт', date = now, type = 'добавлена автоматически при изменении статуса')
+    if instance.status == 'С':
+      CommentsEquipment.objects.create(forNote = instance, note = 'Списано', date = now, type = 'добавлена автоматически при изменении статуса')
+    if instance.status == 'Р':
+      CommentsEquipment.objects.create(forNote = instance, note = 'Отправлено на резервное хранение', date = now, type = 'добавлена автоматически при изменении статуса')
 
