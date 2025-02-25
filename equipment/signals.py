fb18edb894f63b0853e55595e8e0c5875609b966
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-02-25 16:49:44 +0300

    Create signals.py

---
+++ equipment/signals.py
@@ -0,0 +1,20 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import *
+
+
+@receiver(post_init, sender=Equipment)
+def change_status_equipment_get_instance(sender, instance, **kwargs):
+  instance._previous_equipment = instance.equipment
+
+
+@receiver(post_save, sender=Equipment, dispatch_uid="update_stock_count")
+def change_status_equipment(sender, instance, created,**kwargs):
+  if instance._previous_equipment.status != instance.equipment.status:
+    a = CommentsEquipment.objects.get(forNote = instance)
+    a.note = 'поменяли статус'
+    a.save()
+    instance.save()
+        
+
