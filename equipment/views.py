fc4327f952d92059081603168f9e5f1268e375a8
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-03-09 17:46:56 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -3038,17 +3038,20 @@ def BulkDownload(request):
         try:
             file = request.FILES['MeasurEquipmentCharakters_file']
             uploading_file = UploadingMeasurEquipmentCharakters({'file': file})
+            number_objects = uploading_file.number_objects
+            number_rows = uploading_file.number_rows
         except:
             pass
 
         try: 
             file = request.FILES['TestingEquipmentCharakters_file']
             uploading_file = UploadingTestingEquipmentCharakters({'file': file})
+            number_objects = uploading_file.number_objects
+            number_rows = uploading_file.number_rows
         except:
             pass
             
-        number_objects = uploading_file.number_objects
-        number_rows = uploading_file.number_rows
+
         if uploading_file:
             if number_objects and number_rows:
                 messages.success(request, f"Файл успешно загружен, добавлено {number_objects} записей из {number_rows}")
