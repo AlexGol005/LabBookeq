8e7479ae87d3bddafd4424bdccfeff5a91d885d4
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-03-09 20:12:09 +0300

    Update models.py

--- equipment/models.py
+++ equipment/models.py
@@ -312,7 +312,7 @@ class HelpingEquipmentCharakters(models.Model):
     setplace = models.CharField('описание мероприятий по установке', max_length=1000, default='', blank=True, null=True)
     needsetplace = models.BooleanField('Требуется установка (да - "1", нет - "0")', default=False, blank=True)
     complectlist = models.CharField('Где указана комплектация оборудования', max_length=100, default='', blank=True, null=True)
-    expresstest = models.BooleanField('Возможно тестирование (да - "1", нет - "0")', default=False, blank=True)
+    expresstest = models.BooleanField('Возможно тестирование  (да - "1", нет - "0")', default=False, blank=True)
     aim = models.CharField('примечание', max_length=90, blank=True, null=True)
     pointer =  models.CharField('ID добавившей организации', max_length=500, blank=True, null=True) 
 
