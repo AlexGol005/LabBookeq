17272e2573abb014233d8548268b277a9671e506
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-03-14 17:14:43 +0300

    Update function_for_equipmentapp.py

--- equipment/function_for_equipmentapp.py
+++ equipment/function_for_equipmentapp.py
@@ -6,10 +6,10 @@ from users.models import Company
 from equipment.models import Equipment
 
 
-def get_exnumber(request, have_exnumber):
+def get_exnumber(request, have_exnumber, pointer):
     for_exnamber_tail = Company.objects.get(userid=request.user.profile.userid).pk
     try:
-        a = Equipment.objects.filter(exnumber__startswith=have_exnumber).filter(pointer=request.user.profile.userid).last().exnumber
+        a = Equipment.objects.filter(exnumber__startswith=have_exnumber).filter(pointer=pointer).last().exnumber
         b = int(str(a)[1:5]) + 1
         c = str(b).rjust(4, '0')
         d = str(have_exnumber) + c + '_' + str(for_exnamber_tail)
