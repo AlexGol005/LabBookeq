c5b731f5612de25964618c653339664325aae403
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-03-14 13:22:30 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -3153,10 +3153,10 @@ def BulkDownload(request):
     """template_name = URL + '/bulk_download.html'"""
 
     if request.POST:
-            MeasurEquipmentCharakters_file = request.FILES.get('MeasurEquipmentCharakters_file')
-            TestingEquipmentCharakters_file = request.FILES.get('TestingEquipmentCharakters_file')
-            HelpingEquipmentCharakters_file = request.FILES.get('HelpingEquipmentCharakters_file')
-            MeasurEquipment_Equipment_file = request.FILES.get('MeasurEquipment_Equipment_file')
+        MeasurEquipmentCharakters_file = request.FILES.get('MeasurEquipmentCharakters_file')
+        TestingEquipmentCharakters_file = request.FILES.get('TestingEquipmentCharakters_file')
+        HelpingEquipmentCharakters_file = request.FILES.get('HelpingEquipmentCharakters_file')
+        MeasurEquipment_Equipment_file = request.FILES.get('MeasurEquipment_Equipment_file')
         
         if MeasurEquipmentCharakters_file:
             try:
