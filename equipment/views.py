94e0933d91c07227e7c9d3ef408c1444515f304a
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-03-17 17:28:30 +0300

    Update views.py

--- equipment/views.py
+++ equipment/views.py
@@ -3133,7 +3133,7 @@ class UploadingTwoModels(object):
             try:   
                 b, created = self.model2.objects.get_or_create(**row_dict_characters)
                 row_dict_item_metehe['charakters'] = b
-                if created:
+                if b.id:
                     self.number_objects_char+=1
             except:
                 raise Exception(f"проблема в создании/нахождении характеристик {self.kategory_e}: {row_dict_characters}")
@@ -3173,17 +3173,17 @@ class UploadingTwoModels(object):
             except:
                 raise Exception(f"проблема в создании единицы {self.kategory_e}: {row_dict_item_metehe}")
 
-            for column in range(self.num_e, self.num_e + 1):
-                value = s.cell(row, column).value
-                field_name = 'roomnumber'
-                related_model = Rooms         
-                instance_room, created = related_model.objects.get_or_create(roomnumber=value)
-                value = instance_room                          
-                row_dict_room['roomnumber'] = value
-                try:
-                    Roomschange.objects.get_or_create(**row_dict_room)
-                except:
-                    raise Exception(f"проблема в создании Комнаты: {row_dict_room}")
+            # for column in range(self.num_e, self.num_e + 1):
+            #     value = s.cell(row, column).value
+            #     field_name = 'roomnumber'
+            #     related_model = Rooms         
+            #     instance_room, created = related_model.objects.get_or_create(roomnumber=value)
+            #     value = instance_room                          
+            #     row_dict_room['roomnumber'] = value
+            #     try:
+            #         Roomschange.objects.get_or_create(**row_dict_room)
+            #     except:
+            #         raise Exception(f"проблема в создании Комнаты: {row_dict_room}")
                              
         self.number_objects = f'{self.number_objects} единиц ЛО, {self.number_objects_char} характеристик {self.kategory_e}, {self.number_objects_metehe} единиц {self.kategory_e}'
         self.number_rows = s.nrows - 1
