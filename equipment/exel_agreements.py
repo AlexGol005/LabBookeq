aff32595eae7c5d8b76384820c00db68b150a17c
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-03-27 15:48:07 +0300

    Update exel_agreements.py

--- equipment/exel_agreements.py
+++ equipment/exel_agreements.py
@@ -245,7 +245,7 @@ def export_orderverification_xls(request, object_ids):
         for col_num in range(len(row)):
             ws1.write(row_num, col_num, row[col_num], style_plain_border)
 
-    row_num = 0
+    row_num = 1
     for row in rowsTE:
         row_num += 1
         for col_num in range(len(row)):
