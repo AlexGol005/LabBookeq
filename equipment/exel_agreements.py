abdc831bd19e95af26f4b5fa96ecb110b2ce3d14
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-03-27 15:00:27 +0300

    Update exel_agreements.py

--- equipment/exel_agreements.py
+++ equipment/exel_agreements.py
@@ -185,14 +185,14 @@ def export_orderverification_xls(request, object_ids):
         'measurequipment__charakters__typename',
         'lot',
         'yearmanuf',
-        'manufacturer__CompanyName',
+        'manufacturer__сompanyName',
     )
     rowsTE = noteTE.values_list(
         'testingequipment__charakters__name', 
         'testingequipment__charakters__typename',
         'lot',
         'yearmanuf',
-        'manufacturer__CompanyName', 
+        'manufacturer__сompanyName', 
     )
     columnsME = [
             'Название прибора',
