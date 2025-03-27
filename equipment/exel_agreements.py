33ef47821214e7a4081d2989ce0519fbe13607e9
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-03-27 14:36:56 +0300

    Update exel_agreements.py

--- equipment/exel_agreements.py
+++ equipment/exel_agreements.py
@@ -402,7 +402,7 @@ def export_orderverification_1_xls(request, object_ids):
     ws.col(11).width = 300
 
         # данные
-    rows1 = MeasurEquipment.objects.filter(equipment__pk__in=q).\   
+    rows1 = MeasurEquipment.objects.filter(equipment__pk__in=q).  
     annotate(num=Value('1'),\
              note=Value('поверка'),\
              cod1=Value(''),).\
