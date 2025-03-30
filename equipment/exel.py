5e6252c206ad2b910029ac4914a57e3b7c745f42
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-03-30 14:23:17 +0300

    Update exel.py

--- equipment/exel.py
+++ equipment/exel.py
@@ -6757,7 +6757,7 @@ def export_accanalytica_xls(request):
     for col_num in range(7,10):
         ws.write(row_num, col_num, columnsup[col_num], style_border)
         ws.merge(row_num, row_num, 7,9, style_border)
-    for col_num in range(10, 14):
+    for col_num in range(11, 13):
         ws.write(row_num, col_num, columnsup[col_num], style_border)
               
     row_num += 1
