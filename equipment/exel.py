5380ad6bd3eb84218324f237369730a32b569a33
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-03-30 18:40:46 +0300

    Update exel.py

--- equipment/exel.py
+++ equipment/exel.py
@@ -7220,15 +7220,13 @@ def export_rossaccreditacia_xls(request):
 
 
     rows = MeasurEquipment.objects.annotate(blanc= Value(' '),\
-          name=Concat('charakters__name', Value(', '), 'charakters__typename', Value(', '), 'charakters__reestr', output_field=CharField()),\
-          manuf = Concat('equipment__manufacturer__country', Value(', '), 'equipment__manufacturer__companyName', Value(', '), 'equipment__yearmanuf', output_field=CharField()),\
-          exp = Concat('equipment__yearintoservice', Value(', зав. № '), 'equipment__lot'), output_field=CharField()),\
-          metro = Concat('newcertnumber', Value(', от'), 'newdate', Value(' до '), 'newdatedead', output_field=CharField()),\
-                                            
-                                           ).\
-        filter(equipment__pointer=request.user.profile.userid).\
-        filter(equipment__status='Э').\
-        values_list(
+          name=Concat('charakters__name', Value(', '), 'charakters__typename', Value(', '), 'charakters__reestr'),\
+          manuf = Concat('equipment__manufacturer__country', Value(', '), 'equipment__manufacturer__companyName', Value(', '), 'equipment__yearmanuf'),\
+          exp = Concat('equipment__yearintoservice', Value(', зав. № '), 'equipment__lot')),\
+          metro = Concat('newcertnumber', Value(', от'), 'newdate', Value(' до '), 'newdatedead', output_field=CharField())).\
+          filter(equipment__pointer=request.user.profile.userid).\
+          filter(equipment__status='Э').\
+          values_list(
             'equipment__aim',
             'name',
             'manuf',
