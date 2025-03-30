e2792e54e647ef429b5b552bb7f67b80c2d9aafd
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-03-30 18:42:41 +0300

    Update exel.py

--- equipment/exel.py
+++ equipment/exel.py
@@ -7220,10 +7220,11 @@ def export_rossaccreditacia_xls(request):
 
 
     rows = MeasurEquipment.objects.annotate(blanc= Value(' '),\
-          name=Concat('charakters__name', Value(', '), 'charakters__typename', Value(', '), 'charakters__reestr'),\
-          manuf = Concat('equipment__manufacturer__country', Value(', '), 'equipment__manufacturer__companyName', Value(', '), 'equipment__yearmanuf'),\
-          exp = Concat('equipment__yearintoservice', Value(', зав. № '), 'equipment__lot')),\
-          metro = Concat('newcertnumber', Value(', от'), 'newdate', Value(' до '), 'newdatedead', output_field=CharField())).\
+          # name=Concat('charakters__name', Value(', '), 'charakters__typename', Value(', '), 'charakters__reestr'),\
+          # manuf = Concat('equipment__manufacturer__country', Value(', '), 'equipment__manufacturer__companyName', Value(', '), 'equipment__yearmanuf'),\
+          # exp = Concat('equipment__yearintoservice', Value(', зав. № '), 'equipment__lot')),\
+          # metro = Concat('newcertnumber', Value(', от'), 'newdate', Value(' до '), 'newdatedead', output_field=CharField()).\
+        ).\
           filter(equipment__pointer=request.user.profile.userid).\
           filter(equipment__status='Э').\
           values_list(
