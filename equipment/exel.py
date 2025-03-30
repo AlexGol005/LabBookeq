aef8e49100044b719460af7f9989e6633adc0254
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-03-30 18:44:12 +0300

    Update exel.py

--- equipment/exel.py
+++ equipment/exel.py
@@ -7221,8 +7221,8 @@ def export_rossaccreditacia_xls(request):
 
     rows = MeasurEquipment.objects.annotate(blanc= Value(' '),\
           name=Concat('charakters__name', Value(', '), 'charakters__typename', Value(', '), 'charakters__reestr'),\
-          # manuf = Concat('equipment__manufacturer__country', Value(', '), 'equipment__manufacturer__companyName', Value(', '), 'equipment__yearmanuf'),\
-          # exp = Concat('equipment__yearintoservice', Value(', зав. № '), 'equipment__lot')),\
+          manuf = Concat('equipment__manufacturer__country', Value(', '), 'equipment__manufacturer__companyName', Value(', '), 'equipment__yearmanuf'),\
+          exp = Concat('equipment__yearintoservice', Value(', зав. № '), 'equipment__lot')),\
           # metro = Concat('newcertnumber', Value(', от'), 'newdate', Value(' до '), 'newdatedead', output_field=CharField()).\
         ).\
           filter(equipment__pointer=request.user.profile.userid).\
