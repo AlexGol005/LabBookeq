e67435cbf1367fc27c0d216472165b781dad39a3
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-03-30 20:47:13 +0300

    Update exel.py

--- equipment/exel.py
+++ equipment/exel.py
@@ -3507,7 +3507,8 @@ def export_verificlabel_xls(request):
         except:
             pass
         try:
-            n = n + '_' + str(company.pk)
+            # n = n + '_' + str(company.pk)
+            n = 'Т0001_1'
             TestingEquipment.objects.filter(pointer=ruser).get(equipment__exnumber=n)
             note.append(TestingEquipment.objects.filter(pointer=ruser).get(equipment__exnumber=n))
         except:
