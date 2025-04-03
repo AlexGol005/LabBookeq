92cc9a3f79520ac7611b5911c3ca92850cbac7cb
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-03 17:04:42 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models  
 from django.contrib.auth.models import User
 from django_currentuser.db.models import CurrentUserField
-
+from decimal import *
 
 from PIL import  Image
 
