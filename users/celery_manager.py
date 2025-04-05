5ca04d01c352e8a1a05ab0c09a28a7945f8cd552
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-05 11:19:17 +0300

    Create celery_manager.py

---
+++ users/celery_manager.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django_celery_beat.models import PeriodicTask, IntervalScedule
+from .tasks import take_rent
+
+def schedule_take_rent(request):
+  interval, _ = IntervalScedule.objects.get_or_create(every=600, period=IntervalScedule.SECONDS)
+  PeriodicTask.objects.create(interval=interval, name="Ежемесячная автооплата", task="users.tasks.take_rent")
+return HttpResponse(f'123-{a}-{b}-{c}')
+  
