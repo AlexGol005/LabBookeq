e2eb62f6d9322d3e4df639b681aa9e2426dfe844
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-05 13:59:16 +0300

    Update celery.py

--- LabJournal/celery.py
+++ LabJournal/celery.py
@@ -3,9 +3,9 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'example.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LabJournal.settings')
 
-app = Celery('example')
+app = Celery('LabJournal')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
