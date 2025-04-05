f482d66967c2633c345aeb1c93d02d66746d6f15
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-05 09:45:37 +0300

    Update tasks.py

--- users/tasks.py
+++ users/tasks.py
@@ -6,7 +6,7 @@ from .models import monthly_payment, Company, CompanyBalanceChange
 now = date.today()
 
 @shared_task
-def take_rent():
+def take_rent(request):
   note_list = Company.objects.filter(payement_date=now)
   for i in note_list:
     CompanyBalanceChange.objects.create(company=i, reason='Автоматическое списание ежемесячного платежа', amount=monthly_payment)
