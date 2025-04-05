3dd85da71c96ada6364d03421e6df12f9a986221
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-05 12:56:25 +0300

    Update celery_manager.py

--- users/celery_manager.py
+++ users/celery_manager.py
@@ -6,7 +6,7 @@ nowtime = datetime.today().isoformat()
 def schedule_take_rent(request):
   a=0
   interval, _ = IntervalSchedule.objects.get_or_create(every=10, period=IntervalSchedule.SECONDS)
-  PeriodicTask.objects.get_or_create(interval=interval, name="Ежемесячная автооплата", task="users.tasks.take_rent")
+  PeriodicTask.objects.get_or_create(interval=interval, name="Ежемесячная автооплата 10", task="users.tasks.take_rent")
   a+=1
   b = nowtime
   return HttpResponse(f'123-{a}--{b}')
