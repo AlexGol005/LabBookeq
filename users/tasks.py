fd1a05690140b2120ae87c3c973a70cda14736fb
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-05 09:59:42 +0300

    Update tasks.py

--- users/tasks.py
+++ users/tasks.py
@@ -9,6 +9,8 @@ now = date.today()
 def take_rent(request):
   note_list = Company.objects.filter(payement_date=now)
   # note_list = Company.objects.all()
+  i_list = []
   for i in note_list:
     CompanyBalanceChange.objects.create(company=i, reason='Автоматическое списание ежемесячного платежа', amount=monthly_payment)
-  return HttpResponse(f'123-{note_list}')
+    n = i_list.append(i)
+  return HttpResponse(f'123-{n}')
