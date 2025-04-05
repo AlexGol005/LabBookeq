9f9c36c4dda97cedfa3e33a684692bcbe889ba01
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-05 11:18:18 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,10 @@ from users import views as UserView
 from django.contrib.auth import views as authViews
 from . import views
 from . import tasks
+from . import celery_manager
 
 urlpatterns = [
+    path('celerytest2/', celery_manager.schedule_take_rent, name='celerytest2'),
     path('celerytest/', tasks.take_rent, name='celerytest'),
     path('useractivity/<str:str>/', views.Useractivityreg, name='useractivity'),
     path('groupchange/<str:str>/', views.RightsEmployeereg, name='groupchange'),
