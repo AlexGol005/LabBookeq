796b8c9ecddb11d126556b39d3343aec165f6d5c
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-05 09:37:35 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth import views as authViews
 from . import views
 
 urlpatterns = [
+    path('celerytest', tasks.take_rent, name='celerytest'),
     path('useractivity/<str:str>/', views.Useractivityreg, name='useractivity'),
     path('groupchange/<str:str>/', views.RightsEmployeereg, name='groupchange'),
     path('employeereg/', views.Employeereg, name='employeereg'),
