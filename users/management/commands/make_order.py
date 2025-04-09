730294182f6afb9e84d14016cd201c2e46686c52
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-09 14:42:04 +0300

    Update make_order.py

--- users/management/commands/make_order.py
+++ users/management/commands/make_order.py
@@ -1,13 +1,7 @@
-from django.utils import timezone
-from django.core.management.base import BaseCommand, CommandError
-from django_celery_beat.models import PeriodicTask, IntervalSchedule
-from setups.models import Order
+from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
+    help = 'The Zen of Python'
+
     def handle(self, *args, **options):
-			PeriodicTask.objects.create(
-					name='Repeat',
-					task='repeat_minus',
-					interval=IntervalSchedule.objects.get(every=60, period='seconds'),
-          start_time=timezone.now(),
-				)
+        print('fff')
