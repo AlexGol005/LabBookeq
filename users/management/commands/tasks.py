a6302667389e47522cbeeb4c9e5b59daba2b9746
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-09 14:57:48 +0300

    Update and rename make_order.py to tasks.py

---
+++ users/management/commands/tasks.py
@@ -0,0 +1,36 @@
+from datetime import timedelta, date, datetime
+
+from django.core.management.base import BaseCommand
+
+from users.models import monthly_payment, Company, CompanyBalanceChange
+
+now = date.today()
+nowtime = datetime.today().isoformat()
+
+
+class Command(BaseCommand):
+
+
+    def take_rent(self):
+      note_list = Company.objects.filter(payement_date=now)
+      for i in note_list:
+        if i.balance >=monthly_payment:
+          CompanyBalanceChange.objects.create(company=i, reason='Автоматическое списание ежемесячного платежа', amount=-monthly_payment)
+          i.payement_date = i.payement_date + timedelta(days=30)
+          i.pay = True
+          i.save()
+        else:
+          i.payement_date = i.payement_date + timedelta(days=1)
+          i.pay = False
+          i.save()
+        print('оплата проверена, списана')
+
+
+    def access_restriction(self):
+      note_list = Company.objects.filter(pay=False)
+      for i in note_list:
+        instance=User.objects.get(pk=str)
+        instance.is_active = False
+        pass
+
+
