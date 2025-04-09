3d62fbccbd22d12a31256cba8bc1f5da3c7adb0a
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-09 07:00:11 +0300

    Update tasks.py

--- users/tasks.py
+++ users/tasks.py
@@ -5,7 +5,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LabJournal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE')
 
 application = get_wsgi_application()
 import sys
