501ec814d15eff46e2c755ca4b9e2605d2b5d25e
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-10 10:43:36 +0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -372,9 +372,9 @@ def Useractivityreg(request, slug):
             au_list = str(au.list_employees)
             au_list = au_list.split(", ")
             # au_list = au_list.remove(instance.pk)
-            au_list1 = au_list.append(len(au_list))
+            au_list = au_list.append('12')
             # a=', '.join(au_list)
-            au.list_employees = au_list1
+            au.list_employees = au_list
             au.save()
                    
             # except:
