bcdea97942d634075a627f97108e09a88f0296b7
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-10 17:14:25 +0300

    Update tasks.py

--- users/management/commands/tasks.py
+++ users/management/commands/tasks.py
@@ -1,5 +1,5 @@
 from datetime import timedelta, date, datetime
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.core.management.base import BaseCommand
 
 from users.models import *
