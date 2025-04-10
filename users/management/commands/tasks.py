296efc78daea96ab5b627fb06a79b9bbb64faaa4
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-10 13:54:42 +0300

    Update tasks.py

--- users/management/commands/tasks.py
+++ users/management/commands/tasks.py
@@ -24,7 +24,25 @@ class Command(BaseCommand):
                 i.payement_date = i.payement_date + timedelta(days=1)
                 i.pay = False
                 not_pay_i.append(i)
-                i.save()   
+                i.save() 
+        not_pay_companies = CompanyActiveEmployesLists.objects.filter(company__in=not_pay_i)
+        pay_companies = CompanyActiveEmployesLists.objects.filter(company__in=pay_i)
+        for j in not_pay_companies:
+            j.list_employees
+            j_list = str(j.list_employees)
+            j_list = au_list.split(", ")
+            u = Users.objects.filter(pk__in=j_list)
+            for f in u:
+                f.is_active = False
+                f.save()
+         for j in pay_companies:
+            j.list_employees
+            j_list = str(j.list_employees)
+            j_list = au_list.split(", ")
+            u = Users.objects.filter(pk__in=j_list)
+            for f in u:
+                f.is_active = True
+                f.save()
         print(f'Автоматическое списание. Не оплачено: {not_pay_i}. Оплачено: {pay_i}')
 
 
