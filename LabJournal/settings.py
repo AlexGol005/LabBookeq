4d7cebec77fc5b87341ca2ea0f94b516363cba97
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-14 16:54:54 +0300

    Update settings.py

--- LabJournal/settings.py
+++ LabJournal/settings.py
@@ -128,12 +128,27 @@ WSGI_APPLICATION = 'LabJournal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'JL',
+        'USER': 'userjl',
+        'PASSWORD': 'RnES8TPDi',
+        'HOST': 'LabJournal-4495.postgres.pythonanywhere-services.com',
+        'PORT': 14495,
     }
 }
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
